c3cb705ab949c661f1d71328f6a9ba19c1a13793
ASAPRock <moneylordg@gmail.com>
2019-04-24 19:31:01 +0900

    Asset Upload

---
+++ Assets/ImageCount.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoroutineSample : MonoBehaviour {
+    
+    IEnumerator WaitAndPrint()
+    {
+        // suspend execution for 5 seconds
+        yield return new WaitForSeconds(5);
+        print("WaitAndPrint " + Time.time);
+    }
+
+    IEnumerator Start()
+    {
+        print("Starting " + Time.time);
+
+        // Start function WaitAndPrint as a coroutine
+        yield return StartCoroutine("WaitAndPrint");
+        print("Done " + Time.time);
+    }
+}
---
+++ Assets/Launcher.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Diagnostics;
+
+public class Launcher : MonoBehaviour
+{
+
+    public string ExecutePath;
+    public string YoutubeUrl;
+
+    public void Player_Game()
+    {
+        Process.Start(ExecutePath);
+    }
+
+    public void Quit_Launcher()
+    {
+        Application.Quit();
+    }
+
+    public void Open_Youtube()
+    {
+        Application.OpenURL(YoutubeUrl);        
+    }
+}
